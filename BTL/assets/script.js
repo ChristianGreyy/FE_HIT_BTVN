e5ace8b8b4069916ccc8e1cc31e9b865536a6985
Christian Grey <truongthanhhung2k2@gmail.com>
2021-12-10 23:00:27 +0700

    Update Radio Zing Mp3

--- BTL/assets/script.js
+++ BTL/assets/script.js
@@ -167,3 +167,20 @@ new Chart("myChart", {
     legend: {display: false}
   }
 });
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:1
+        }
+    }
+})
